b20cc56f2118484ffa7bae3cdf845e307b933fe0
Luca Marchisio <marchisioluca15@gmail.com>
2026-01-31 11:27:09 +0100

    Modified cmake + added example

---
+++ examples/example1.c
@@ -0,0 +1,11 @@
+#include <clog/log.h>
+
+int log_level = LOG_INFO;
+
+int main(void) {
+    log_init();
+    printl(LOG_ERR, "This is an error!");
+    printl(LOG_WARN, "This is a warn!");
+    printl(LOG_INFO, "This is an info!");
+    return 0;
+}
